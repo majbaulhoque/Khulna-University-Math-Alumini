53b18302a607dd70697cb081926532b94e5a6921
majbaulhoque <majba9399@gmail.com>
2024-07-26 22:25:31 +0600

    added card in gallery page

--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -1,15 +1,93 @@
-
+import Cards from "../../assets/Images/card.jpg"
 
 const Gallery = () => {
     return (
-        <div>
+        <div style={{marginTop: "80px"}}>
             <div className="container">
-                <div className="row">
-                    <div className="col-3">
-                        
+                <div className="row my-5">
+                    <div className="card-container position-relative col-12 col-md-6 col-lg-4 my-md-5 my-lg-5">
+                        <div className="img-box position-relative">
+                            <img className="image" src={Cards} alt="Gallery" />
+                        </div>
+                        <div className="content">
+                            <h3>Camera</h3>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum, nobis sunt. Sapiente maiores aliquam consequuntur iste odit sed excepturi libero!</p>
+                        </div>
                     </div>
-                    <div className="col-3"></div>
-                    <div className="col-3"></div>
+                    <div className="card-container position-relative col-12 col-md-6 col-lg-4 my-md-5 my-lg-5">
+                        <div className="img-box position-relative">
+                            <img className="image" src={Cards} alt="Gallery" />
+                        </div>
+                        <div className="content">
+                            <h3>Camera</h3>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum, nobis sunt. Sapiente maiores aliquam consequuntur iste odit sed excepturi libero!</p>
+                        </div>
+                    </div>
+                    <div className="card-container position-relative col-12 col-md-6 col-lg-4 my-md-5 my-lg-5">
+                        <div className="img-box position-relative">
+                            <img className="image" src={Cards} alt="Gallery" />
+                        </div>
+                        <div className="content">
+                            <h3>Camera</h3>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum, nobis sunt. Sapiente maiores aliquam consequuntur iste odit sed excepturi libero!</p>
+                        </div>
+                    </div>
+                    <div className="card-container position-relative col-12 col-md-6 col-lg-4 my-md-5 my-lg-5">
+                        <div className="img-box position-relative">
+                            <img className="image" src={Cards} alt="Gallery" />
+                        </div>
+                        <div className="content">
+                            <h3>Camera</h3>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum, nobis sunt. Sapiente maiores aliquam consequuntur iste odit sed excepturi libero!</p>
+                        </div>
+                    </div>
+                    <div className="card-container position-relative col-12 col-md-6 col-lg-4 my-md-5 my-lg-5">
+                        <div className="img-box position-relative">
+                            <img className="image" src={Cards} alt="Gallery" />
+                        </div>
+                        <div className="content">
+                            <h3>Camera</h3>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum, nobis sunt. Sapiente maiores aliquam consequuntur iste odit sed excepturi libero!</p>
+                        </div>
+                    </div>
+                    <div className="card-container position-relative col-12 col-md-6 col-lg-4 my-md-5 my-lg-5">
+                        <div className="img-box position-relative">
+                            <img className="image" src={Cards} alt="Gallery" />
+                        </div>
+                        <div className="content">
+                            <h3>Camera</h3>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum, nobis sunt. Sapiente maiores aliquam consequuntur iste odit sed excepturi libero!</p>
+                        </div>
+                    </div>
+                    <div className="card-container position-relative col-12 col-md-6 col-lg-4 my-md-5 my-lg-5">
+                        <div className="img-box position-relative">
+                            <img className="image" src={Cards} alt="Gallery" />
+                        </div>
+                        <div className="content">
+                            <h3>Camera</h3>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum, nobis sunt. Sapiente maiores aliquam consequuntur iste odit sed excepturi libero!</p>
+                        </div>
+                    </div>
+                    <div className="card-container position-relative col-12 col-md-6 col-lg-4 my-md-5 my-lg-5">
+                        <div className="img-box position-relative">
+                            <img className="image" src={Cards} alt="Gallery" />
+                        </div>
+                        <div className="content">
+                            <h3>Camera</h3>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum, nobis sunt. Sapiente maiores aliquam consequuntur iste odit sed excepturi libero!</p>
+                        </div>
+                    </div>
+                    <div className="card-container position-relative col-12 col-md-6 col-lg-4 my-md-5 my-lg-5">
+                        <div className="img-box position-relative">
+                            <img className="image" src={Cards} alt="Gallery" />
+                        </div>
+                        <div className="content">
+                            <h3>Camera</h3>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum, nobis sunt. Sapiente maiores aliquam consequuntur iste odit sed excepturi libero!</p>
+                        </div>
+                    </div>
+                    
+                    
                 </div>
             </div>
         </div>
