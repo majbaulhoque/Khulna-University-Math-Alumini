668c4cc7c28bcf79760364ca825854bf5c689006
Majba UL Hoque <majba9399@gmail.com>
2024-08-14 22:35:57 +0600

    Tab Reload issue
    using NavLink and replace href with to

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { Container, Nav } from "react-bootstrap";
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { useLocation } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import Logo from '../Logo/Logo';
 
 const NavBar = () => {
@@ -25,21 +25,20 @@ const NavBar = () => {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto" activeKey={activeKey} onSelect={(selectedKey) => setActiveKey(selectedKey)}>
-                            <Nav.Link href="/" className={activeKey === '/' ? 'active' : ''}>Home</Nav.Link>
-                            <Nav.Link href="/discipline" className={activeKey === '/discipline' ? 'active' : ''}>Discipline</Nav.Link>
+                            <Nav.Link as={NavLink} to="/" className={activeKey === '/' ? 'active' : ''}>Home</Nav.Link>
+                            <Nav.Link as={NavLink} to="/discipline" className={activeKey === '/discipline' ? 'active' : ''}>Discipline</Nav.Link>
                             <NavDropdown title="Member" id="about-us-dropdown">
-                                <NavDropdown.Item href="/applyForMembership" className={activeKey === '/applyForMembership' ? 'active' : ''}>Apply for Membership</NavDropdown.Item>
-                                <NavDropdown.Item href="/memberList" className={activeKey === '/memberList' ? 'active' : ''}>Member List</NavDropdown.Item>
-
+                                <NavDropdown.Item as={NavLink} to="/applyForMembership" className={activeKey === '/applyForMembership' ? 'active' : ''}>Apply for Membership</NavDropdown.Item>
+                                <NavDropdown.Item as={NavLink} to="/memberList" className={activeKey === '/memberList' ? 'active' : ''}>Member List</NavDropdown.Item>
                             </NavDropdown>
-                            <Nav.Link href="/publication" className={activeKey === '/publication' ? 'active' : ''}>Publication</Nav.Link>
+                            <Nav.Link as={NavLink} to="/publication" className={activeKey === '/publication' ? 'active' : ''}>Publication</Nav.Link>
                             <NavDropdown title="About US" id="about-us-dropdown">
-                                <NavDropdown.Item href="/trustees" className={activeKey === '/trustees' ? 'active' : ''}>Board of Trustees</NavDropdown.Item>
-                                <NavDropdown.Item href="/ecCouncil" className={activeKey === '/ecCouncil' ? 'active' : ''}>EC Council</NavDropdown.Item>
+                                <NavDropdown.Item as={NavLink} to="/trustees" className={activeKey === '/trustees' ? 'active' : ''}>Board of Trustees</NavDropdown.Item>
+                                <NavDropdown.Item as={NavLink} to="/ecCouncil" className={activeKey === '/ecCouncil' ? 'active' : ''}>EC Council</NavDropdown.Item>
                             </NavDropdown>
-                            <Nav.Link href="/contact" className={activeKey === '/contact' ? 'active' : ''}>Contact</Nav.Link>
-                            <Nav.Link href="/gallery" className={activeKey === '/gallery' ? 'active' : ''}>Gallery</Nav.Link>
-                            <Nav.Link href="/login" className={activeKey === '/login' ? 'active' : ''}>Log In</Nav.Link>
+                            <Nav.Link as={NavLink} to="/contact" className={activeKey === '/contact' ? 'active' : ''}>Contact</Nav.Link>
+                            <Nav.Link as={NavLink} to="/gallery" className={activeKey === '/gallery' ? 'active' : ''}>Gallery</Nav.Link>
+                            <Nav.Link as={NavLink} to="/login" className={activeKey === '/login' ? 'active' : ''}>Log In</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
